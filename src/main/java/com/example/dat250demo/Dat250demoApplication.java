d6b3b8032d7f453e06386acc635ca0b5b6ee983a
599061 <dag__aa@hotmail.com>
2025-09-06 12:22:03 +0200

    First commit

---
+++ src/main/java/com/example/dat250demo/Dat250demoApplication.java
@@ -0,0 +1,13 @@
+package com.example.dat250demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Dat250demoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Dat250demoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/dat250demo/Dat250demoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dat250demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Dat250demoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
